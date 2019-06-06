464747f6ae5021ff8632f03c5f93beb889a2da53
Tomas Harkema <tomas@harkema.io>
2019-06-06 10:51:52 +0200

    add test path

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
             path: "Sources"),
         .testTarget(
             name: "DeepDiffTests",
-            dependencies: ["DeepDiff"]),
+            dependencies: ["DeepDiff"],
+            path: "DeepDiffTests"),
     ]
 )
